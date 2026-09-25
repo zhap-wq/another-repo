dede98e93a599dc66ca6ae36b98596d95c10aa0a
zhap-wq <zhap@bc.edu>
2026-09-25 18:08:01 -0400

    Clean up price printing examples
    Removed examples of string concatenation and formatted strings.

--- Class 9.11.py
+++ Class 9.11.py
@@ -11,13 +11,4 @@ print(n)
 price =3
 print("the price with tax is $", price)
 
-#a second way, string concactination
-price_1=6
-print("the price with tax is $"+ str(price))
 
-#third way, formatted string
-price_2=9.9
-print(f'The price with tax is ${price:.2f}\n')
-
-# * can also used for repeat strings
-print("hi"*3)
